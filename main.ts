488f69151454e47a57e51632e2f7090952b3059f
David Morgantini <dmorgantini@gmail.com>
2025-11-22 17:16:31 -0800

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -6,17 +6,22 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         mySprite.vy = -150
     }
 })
+function addEnemy (x: number, y: number) {
+    mySprite2 = sprites.create(assets.image`Hit Box`, SpriteKind.Enemy)
+    mySprite2.setPosition(x, y)
+}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-	
+    game.over(false, effects.dissolve)
 })
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
 scene.setBackgroundColor(8)
 tiles.setTilemap(tilemap`level1`)
 mySprite = sprites.create(assets.image`GD Square`, SpriteKind.Player)
-for (let index = 0; index < 10; index++) {
-    mySprite2 = sprites.create(assets.image`Hit Box`, SpriteKind.Enemy)
-}
+mySprite.setFlag(SpriteFlag.ShowPhysics, true)
+addEnemy(105, 105)
+addEnemy(265, 105)
+addEnemy(290, 105)
 mySprite.setPosition(6, 98)
 mySprite.ay = 400
 mySprite.vx = 100
