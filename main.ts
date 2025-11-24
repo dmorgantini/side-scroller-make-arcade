a8694f063090833ac14e6b7159b071e6cff9f5be
lumorgantini <lumorgantini@gmail.com>
2025-11-24 08:42:42 -0800

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,28 @@
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
-    game.over(true, effects.confetti)
-})
+namespace SpriteKind {
+    export const ending = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
         mySprite.vy = -150
+        if (direction == "up") {
+            mySprite.vy = -150
+            timer.after(100, function () {
+                mySprite.setImage(assets.image`GD Square0`)
+                timer.after(100, function () {
+                    mySprite.setImage(assets.image`GD Square1`)
+                    direction = "down"
+                })
+            })
+        } else {
+            timer.after(100, function () {
+                mySprite.setImage(assets.image`GD Square2`)
+                timer.after(100, function () {
+                    mySprite.setImage(assets.image`GD Square`)
+                    direction = "up"
+                })
+            })
+        }
     }
-})
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.setImage(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
@@ -50,87 +48,21 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 function addSpike (x: number) {
-    mySprite2 = sprites.create(assets.image`Hit Box`, SpriteKind.Enemy)
-    mySprite2.setPosition(9 + 16 * x, 105)
+    spike_hit = sprites.create(assets.image`Hit Box`, SpriteKind.Enemy)
+    spike_hit.setPosition(9 + 16 * x, 105)
+    spike_hit.setFlag(SpriteFlag.Invisible, false)
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    game.over(true, effects.confetti)
+})
+function addminSpike (x: number) {
+    spike_hit = sprites.create(assets.image`Hit Box`, SpriteKind.Enemy)
+    spike_hit.setPosition(9 + 16 * x, 110)
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    animation.runImageAnimation(
-    mySprite,
-    [img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . 4 4 . . . . . . . 
-        . . . . . . 4 5 5 4 . . . . . . 
-        . . . . . . 2 5 5 2 . . . . . . 
-        . . . . . . . 2 2 . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . 4 . . . . . 
-        . . . . 2 . . . . 4 4 . . . . . 
-        . . . . 2 4 . . 4 5 4 . . . . . 
-        . . . . . 2 4 d 5 5 4 . . . . . 
-        . . . . . 2 5 5 5 5 4 . . . . . 
-        . . . . . . 2 5 5 5 5 4 . . . . 
-        . . . . . . 2 5 4 2 4 4 . . . . 
-        . . . . . . 4 4 . . 2 4 4 . . . 
-        . . . . . 4 4 . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . 3 . . . . . . . . . . . 4 . . 
-        . 3 3 . . . . . . . . . 4 4 . . 
-        . 3 d 3 . . 4 4 . . 4 4 d 4 . . 
-        . . 3 5 3 4 5 5 4 4 d d 4 4 . . 
-        . . 3 d 5 d 1 1 d 5 5 d 4 4 . . 
-        . . 4 5 5 1 1 1 1 5 1 1 5 4 . . 
-        . 4 5 5 5 5 1 1 5 1 1 1 d 4 4 . 
-        . 4 d 5 1 1 5 5 5 1 1 1 5 5 4 . 
-        . 4 4 5 1 1 5 5 5 5 5 d 5 5 4 . 
-        . . 4 3 d 5 5 5 d 5 5 d d d 4 . 
-        . 4 5 5 d 5 5 5 d d d 5 5 4 . . 
-        . 4 5 5 d 3 5 d d 3 d 5 5 4 . . 
-        . 4 4 d d 4 d d d 4 3 d d 4 . . 
-        . . 4 5 4 4 4 4 4 4 4 4 4 . . . 
-        . 4 5 4 . . 4 4 4 . . . 4 4 . . 
-        . 4 4 . . . . . . . . . . 4 4 . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . b b . b b b . . . . . 
-        . . . . b 1 1 b 1 1 1 b . . . . 
-        . . b b 3 1 1 d d 1 d d b b . . 
-        . b 1 1 d d b b b b b 1 1 b . . 
-        . b 1 1 1 b . . . . . b d d b . 
-        . . 3 d d b . . . . . b d 1 1 b 
-        . b 1 d 3 . . . . . . . b 1 1 b 
-        . b 1 1 b . . . . . . b b 1 d b 
-        . b 1 d b . . . . . . b d 3 d b 
-        . b b d d b . . . . b d d d b . 
-        . b d d d d b . b b 3 d d 3 b . 
-        . . b d d 3 3 b d 3 3 b b b . . 
-        . . . b b b d d d d d b . . . . 
-        . . . . . . b b b b b . . . . . 
-        `],
-    500,
-    true
-    )
-    game.over(false)
+    game.gameOver(false)
 })
-let mySprite2: Sprite = null
+let spike_hit: Sprite = null
 let mySprite: Sprite = null
 let direction = ""
 direction = "down"
@@ -140,7 +72,7 @@ mySprite = sprites.create(assets.image`GD Square`, SpriteKind.Player)
 mySprite.setFlag(SpriteFlag.ShowPhysics, true)
 direction = "up"
 addSpike(6)
-addSpike(16)
+addminSpike(16)
 addSpike(17)
 addSpike(29)
 addSpike(30)
